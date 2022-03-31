7ba17dc0740f2559670446eb48dc8bb5d0ebde7f
harry barcia <barcia.harry@gmail.com>
2022-03-30 23:00:06 -0700

    suite portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react'
+import Logo from '../assets/logo.png';
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import {HiOutLineMail}  from 'react-icons/hi';
+Importt {BsFillPersonLinesFill} from 'react-icons/bs';
 
 const Navbar = () => {
+  const[nav, setNav]=useState(false)
+  const handleClick=()=>setNav(!nav)
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      
+       <div>
+         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+       </div>
+       
+         <ul className='hidden md:flex' >
+           <li>Home</li>
+           <li>About</li>
+           <li>Skills</li>
+           <li>Work</li>
+           <li>Contact</li>
+         </ul>
+       
+
+       {/*hamburger*/}
+       <div onClick={handleClick} className='md:hidden z-10'>
+         {!nav?
+          <FaBars/>: <FaTimes/>
+        }
+        </div>
+         
+       {/*Mobile Menu*/}
+        <ul className={!nav?'hidden':' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className="py-6 text-4xl">Home</li>
+            <li className="py-6 text-4xl">About</li>
+            <li className="py-6 text-4xl">Skills</li>
+            <li className="py-6 text-4xl">Work</li>
+            <li className="py-6 text-4xl">Contact</li>
+        </ul>
+
+       {/*Social icons*/}
+       <div className='flex fixed flex-col top-[35%] left-0'>
+         <ul>
+           <li>
+             <a href="/"></a>
+             Linkedin<FaFacebook size={30}
+           </li>
+           <li>
+           <a href="/"></a></li>
+           <li>
+           <a href="/"></a></li>
+           <li>
+           <a href="/"></a></li>
+         </ul>
+          
+        </div>
+       
     </div>
 
   )
